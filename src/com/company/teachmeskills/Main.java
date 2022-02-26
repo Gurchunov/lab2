9b18dfccb24dd74a2ff8cda8dc043a5a56b85e1f
gurchunov-1997@mail.ru <Flatronl1718s>
2022-02-26 22:59:54 +0300

    homeWork2

---
+++ src/com/company/teachmeskills/Main.java
@@ -0,0 +1,88 @@
+package com.company.teachmeskills;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Task1
+
+//        int[] var = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+//        int sum = 0;
+//        for (int i = 0; i < var.length; i++) {
+//            sum = (sum + i);
+//        }
+//        sum = sum / 10;
+//        System.out.println(sum);
+
+
+        //Task2
+
+//        double x = 0.25;
+//        double y = 0.9;
+//        double a = 10 * x + y;
+//        System.out.println(a);
+//        double b = y * Math.sqrt(x);
+//        System.out.println(b);
+//        x = (Math.pow(x,y))-1;
+//        System.out.println(x);
+
+
+        //Task3
+
+//        int x = 10;
+//        x = (int) Math.sqrt(x);
+//        System.out.println(x);
+
+
+        //Task4
+
+//        int x = 5;
+//        int y = 10;
+//        int z;
+//        x = x*x;
+//        y = y*y;
+//        z = (int) Math.sqrt(x+y);
+//        System.out.println(z);
+
+
+        //Task5
+
+//        int x = 10;
+//        int y = 20;
+//        int z = (int) Math.sqrt(y - x);
+//        z = z + 1;
+//        System.out.println(z);
+
+
+//         Task 6
+
+//        int x= -30;
+//        int y = 40;
+//        int z = -30-40;
+//        z= Math.abs(z);
+//        System.out.println(z);
+
+
+        //Task7
+
+//        int a = 1000;
+//        int b = 500;
+//        int c = (int) ((a + b) * 0.1);
+//        System.out.println("сумма скидки равна: " + c);
+//        c = (a + b) - c;
+//        System.out.println("сумма с учетом скидки: " + c);
+
+
+//        //Task8.1
+//        System.out.println(Main.calculation(200, 30, 15, 10, 2));
+//        //Task8.2
+//        System.out.println(Main.calculation(500, 50, 50, 7, 1));
+//        //Task8.3
+//        System.out.println(Main.calculation(300, 20, 10, 2, 3));
+//    }
+//
+//    public static int calculation(int priceA, int priceB, int persent, int numberA, int numberB) {
+//        persent = (priceA * numberA * persent) / 100;
+//        return ((priceA * numberA) - persent) + (priceB * numberB);
+    }
+}
